797cf214f23ef5905096535c004fd95b4be2b28c
Donavyn <donavynhaley@gmail.com>
2022-10-08 01:02:02 -0700

    Added chess dependencies updated chessboard size

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import Chessboard from "chessboardjsx";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="chess-container">
+                <h1>Chess App</h1>
+                <Chessboard
+                    position={"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}
+                    calcWidth={(screenSize) => {
+                        const spacing = 200
+                        const ratio = (screenSize.screenHeight - spacing) / (screenSize.screenWidth - spacing)
+                        console.log(ratio)
+                        return screenSize.screenWidth * ratio
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
